59bd005aff2b63c6f851e32676525a70706ab897
Roberto Leonie <38226441+robertoleonie@users.noreply.github.com>
2024-07-28 01:33:30 -0300

    2024-07-28

--- approaches.py
+++ approaches.py
@@ -4,6 +4,18 @@ class Approaches() :
     def __init__(self) :
         pass
 
+    def find_indices(self, input_list, element):
+            return [i for i, x in enumerate(input_list) if x == element]
+    
+    def find_sublist_start_index(self, subset, arr):
+        size = len(subset)
+        
+        for i in range(len(arr) - size + 1):
+            if arr[i:i + size] == subset:
+                return i
+        
+        return -1
+
     def completely_random(self, n) :
         facility = [0] * n
         index_list = [i for i in range(0,n)]
@@ -57,6 +69,76 @@ class Approaches() :
 
         return steps
 
+    def individualist_altruistic(self, n) :
+        facility = [0] * n
+        index_list = [i for i in range(0,n)]
+        steps = 0
+
+        coin = random.randint(0,1)
+        if (coin) :
+            seq = [n-1, 0]
+        else :
+            seq = [0, n-1]
+
+        while (seq) :
+            idx = seq.pop(0)
+            facility[idx] = 1
+            if idx == 0:
+                facility[idx + 1] = 'X'
+            else:
+                facility[idx - 1] = 'X'
+            index_list.remove(idx)
+            steps += 1
+
+            print(idx, '\n', facility, '\n', index_list, '\n', steps, '\n')
+
+        while (index_list) :
+            if (self.find_sublist_start_index([0], facility) != -1) :
+                pos = self.find_sublist_start_index(['X', 'X'], facility)
+                print(pos)
+                coin = random.randint(0,1)
+                print(coin)
+                if (pos == -1) :    
+                    if (coin) :
+                        pos = self.find_sublist_start_index([0, 0, 'X'], facility)
+                        print(pos)
+                        if (pos != -1) :
+                            idx = pos+1
+                            facility[idx] = 1
+                            facility[idx-1] = 'X'
+                            index_list.remove(idx)
+
+                            print(idx, '\n', facility, '\n', index_list, '\n', steps, '\n')
+
+                    else :
+                        pos = self.find_sublist_start_index(['X', 0, 0], facility)
+                        print(pos)
+                        if (pos != -1) :
+                            idx = pos+1
+                            facility[idx] = 1
+                            facility[idx+1] = 'X'
+                            index_list.remove(idx)
+
+                            print(idx, '\n', facility, '\n', index_list, '\n', steps, '\n')
+
+                else :
+                    idx = pos + coin
+                    facility[idx] = 1
+                    index_list.remove(idx)
+
+                    print(idx, '\n', facility, '\n', index_list, '\n', steps, '\n')
+
+            else :
+                idx = random.choice(index_list)
+                facility[idx] = 1
+                index_list.remove(idx)
+
+                print(idx, '\n', facility, '\n', index_list, '\n', steps, '\n')
+
+            steps += 1
+
+        return steps
+
     def cleaner(self, n) :
         facility = [0] * n
         index_list = []
@@ -78,8 +160,8 @@ class Approaches() :
         return steps
 
     def discomfort_optimization(self, n, users) :
-        def find_indices(input_list, element):
-            return [i for i, x in enumerate(input_list) if x == element]
+        if (users > n) :
+            raise ValueError("Erro: número de usuários maior do que a capacidade da instalação!\n")
 
         facility = [0] * n
         index_list = [i for i in range(0,n)]
@@ -102,8 +184,8 @@ class Approaches() :
                     neighborhood[n-2] -= 1
                 
                 else :
-                    two_neighbors_indices = find_indices(neighborhood, 2)
-                    one_neighbor_indices = find_indices(neighborhood, 1)
+                    two_neighbors_indices = self.find_indices(neighborhood, 2)
+                    one_neighbor_indices = self.find_indices(neighborhood, 1)
 
                     while (two_neighbors_indices) :
                         idx = random.choice(two_neighbors_indices)
